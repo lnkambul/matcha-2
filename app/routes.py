b386b9e68768c3285639729d14afc0361bc51b0a
gradez <ksefeane@student.wethinkcode.coza>
2020-01-02 10:59:19 +0200

    ok then

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "yis"
+	user = {'username': 'kori'}
+	return render_template('index.html', title='Home', user=user)
