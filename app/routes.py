b5ca41162ae7d005eb29b538db78e482a5908783
gradez <ksefeane@student.wethinkcode.coza>
2020-01-15 22:01:50 +0200

    database setup

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -8,7 +8,10 @@ def index():
 	user = {'name' : 'kori', 'username' : 'ksefeane'}
 	return render_template('index.html', title='home', user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		flash('request user login -> {}'.format(form.username.data))
+		return redirect(url_for('index'))
 	return render_template('login.html', title='log in', form=form)
--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'keeping-secrets-are-we?'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'matcha.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
