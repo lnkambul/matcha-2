e5aa00f61e8d90686763c51cf062aa636e659180
gradez <ksefeane@student.wethinkcode.coza>
2020-01-31 11:55:44 +0200

    restarting again, because ORM

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'keeping-secrets-are-we?'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-		'sqlite:///' + os.path.join(basedir, 'matcha.db')
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
