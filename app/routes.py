d6784e992e6df5f62d4829ff99f5d66bd0917a0b
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-02-14 14:53:17 +0200

    created login form, login html, added url's to base.html

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username': 'kori'}
-	return render_template('index.html', title='home', user=user)
+	return render_template('index.html', title='home')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form=LoginForm()
+	if form.validate_on_submit():
+		flash('{} login request'.format(form.username.data))
+		return redirect(url_for('index'))
+	return render_template('login.html', title='login', form=form)
