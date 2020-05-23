547c7eb2841d8de9f426208c048001a6177de4a0
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-05-23 03:24:24 +0200

    config/tables.js, DB->create_t(), insert() working

---
+++ config/tables.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var tables = {
+	users : (
+		"CREATE TABLE `users` (" +
+		" `id` int(11) NOT NULL AUTO_INCREMENT," +
+		" `username` varchar(20) NOT NULL," +
+		" `email` varchar(60) NOT NULL," +
+		" `password` varchar(100) NOT NULL," +
+		" `verified` bit NOT NULL DEFAULT 0," +
+		" PRIMARY KEY (`id`)" +
+		") ENGINE=InnoDB"
+	),
+	profiles : (
+		"CREATE TABLE `profiles` (" +
+		" `id` int(11) NOT NULL UNIQUE," +
+		" `user_id` int(11)," +
+		" `first_name` varchar(25) NOT NULL," +
+		" `last_name` varchar(25) NOT NULL," +
+		" `gender` varchar(25) NOT NULL," +
+		" `orientation` varchar(25) NOT NULL," +
+		" `preference` varchar(240) NOT NULL," +
+		" `interests` varchar(200) NOT NULL," +
+		" `bio` varchar(255)," +
+		" FOREIGN KEY (`user_id`) REFERENCES users(`id`)" +
+		") ENGINE=InnoDB"
+	),
+	images : (
+		"CREATE TABLE `images` (" +
+		" `id` int(11) NOT NULL UNIQUE," +
+		" `user_id` int(11)," +
+		" `img_src` varchar(250) NOT NULL," +
+		" `pro_pic` varchar(250) NOT NULL," +
+		" FOREIGN KEY (`user_id`) REFERENCES users(`id`)" +
+		") ENGINE=InnoDB"
+	),
+	tokens : (
+		"CREATE TABLE `tokens` (" +
+		" `id` int(11) NOT NULL UNIQUE," +
+		" `user_id` int(11)," +
+		" `token` varchar(100) NOT NULL," +
+		" FOREIGN KEY (`user_id`) REFERENCES users(`id`)" +
+		") ENGINE=InnoDB"
+	)
+}
+
+module.exports = tables;
--- models/db.model.js
+++ models/db.model.js
@@ -2,6 +2,7 @@
 
 const mysql = require('mysql');
 const conf = require ('../config/config');
+const tables = require('../config/tables');
 
 class DB{
 	constructor(config) {
@@ -21,12 +22,13 @@ class DB{
 				});
 			}
 		});
+		this.tables = tables;
 	}
 	
 	create_db() {
 		this.dbc.query(`CREATE DATABASE ${this.db}`, (err, res) => {
 			if (err) {
-				console.log (`BD: ${this.db} -> (already exists)`);
+				console.log (`DB: ${this.db} -> (found)`);
 			}
 			else {
 				console.log (`DB: ${this.db} -> (created)`);
@@ -35,6 +37,7 @@ class DB{
 	};
 
 	init_db() {
+		this.create_db();
 		this.dbc.query(`USE ${this.db}`, (err, res) => {
 			if (err) {
 				this.create_db();
@@ -44,10 +47,36 @@ class DB{
 		});
 	}
 
-	create_t(tables) {
-
+	create_t() {
+		this.init_db();
+		for (let t_name in this.tables) {
+			var sql = this.tables[t_name];
+			this.dbc.query(sql, (err, res) => {
+				var msg = `T: ${t_name} -> `;
+				if (err) {
+					if (err.errno === 1050)
+						msg += "(found)";
+					else
+						console.log (err)
+				}
+				else {
+					msg += "(created)";
+				}
+				console.log (msg);
+			});
+		}
 	}
-}
 
+	insert(sql, values) {
+		this.dbc.query(sql, values, (err, res) => {
+			if (err)
+				console.log (err);
+			else
+				console.log (res);
+		});
+	}
+
+}
 var db = new DB(conf);
 db.init_db();
+db.insert("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", ['kori', 'sefeane', 'gmail']);
