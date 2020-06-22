89d76710ea0a9a54e231e53662221e01cd007499
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-06-22 15:57:42 +0200

    email config

--- models/emailModel.js
+++ models/emailModel.js
@@ -2,6 +2,6 @@ const conf = require('../config/emailconfig')
 const pvt = require('../../emailconfig')
 const nodemailer = require('nodemailer')
 
-const email = nodemailer.createTransport(pvt)
+const email = nodemailer.createTransport(conf)
 
 module.exports = email
