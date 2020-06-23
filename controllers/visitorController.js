02732945e8a59a7fe151543d852940aac67f41d5
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-06-23 13:41:01 +0200

    consistent navbar

--- controllers/visitorController.js
+++ controllers/visitorController.js
@@ -2,6 +2,7 @@ const Q = require('../models/queryModel')
 
 exports.listVisitors = (req, res) => {
 	var token = req.session.token
+	var adminToken = req.session.adminToken
 	var visitors = null
 	Q.fetchone("visits", ['visitor'], ['visited'], [req.session.user], (err, data) => {
 		if (err)
@@ -10,12 +11,17 @@ exports.listVisitors = (req, res) => {
 			visitors = data
 		}
 		else {console.log('no visitors')}
-		res.render('visitors', {token: token, visitors: visitors})
+		res.render('visitors', {
+			token: token, 
+			 visitors: visitors,
+			 adminToken: adminToken
+		})
 	})
 }
 
 exports.listLikes = (req, res) => {
 	var token = req.session.token
+	var adminToken = req.session.adminToken
 	var likes = null
 	Q.fetchone("likes", ['username'], 'liked', req.session.user, (err, data) => {
 		if (err)
@@ -23,7 +29,11 @@ exports.listLikes = (req, res) => {
 		else if (data.length > 0) {
 			likes = data
 		} else {console.log('no one likes you')}
-		res.render('likes', {token: token, likes: likes})
+		res.render('likes', {
+			token: token, 
+			likes: likes,
+			adminToken: adminToken
+		})
 	})
 }
 
