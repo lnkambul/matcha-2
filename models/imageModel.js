cbc4f177e8aba5f3e5ec581e45dd4d0ab7613012
lnkambul <lindonkambule@gmail.com>
2020-06-23 21:57:58 +0200

    stable

--- models/imageModel.js
+++ models/imageModel.js
@@ -1,7 +1,17 @@
 const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 
 const storage = multer.diskStorage({
 	destination: (req, file, callback) => {
+		const tempPath = path.join(__dirname, '../public/uploads')
+		if (!fs.existsSync(tempPath)) {
+			fs.mkdirSync(tempPath)
+			fs.mkdirSync(path.join(tempPath, 'temp'))
+		}
+		else if (!fs.existsSync(path.join(tempPath, 'temp'))) {
+			fs.mkdirSync(path.join(tempPath, 'temp'))
+		}
 		var dest = 'uploads/temp'
 		callback(null, 'public/'+dest)
 	},
