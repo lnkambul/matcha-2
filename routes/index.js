ba6821409801bbb45e5759aedc4e5ddcabb147c6
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-06-26 17:26:57 +0200

    changed index auth to profile.auth for new users to register before access

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
+const profileController = require('../controllers/profileController')
 const visitorController = require('../controllers/visitorController')
 
-router.get('/', userController.auth, userController.list_users)
+router.get('/', profileController.auth, userController.list_users)
 
 //router.get('/:map', userController.auth, visitorController.lost)
 
