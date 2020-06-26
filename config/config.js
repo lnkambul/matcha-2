7654ebe103964b89aac06e855a849e923e0f9ba5
lnkambul <lnkambul@student.wethinkcode.co.za>
2020-06-26 10:42:31 +0200

    responsive buttons

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ var config = {
 	logins: {
 		host: 'localhost',
 		user: 'root',
-		password: 'root'
+		password: ''
 	},
 	db: 'matcha_db'
 }
--- config/emailconfig.js
+++ config/emailconfig.js
@@ -1,8 +1,8 @@
 const elogs = {
 	service: 'gmail',
 	auth: {
-		user: 'lnkambul@student.wethinkcode.co.za',
-		pass: 'dobbyisafreeelf'
+		user: '',
+		pass: ''
 	}
 }
 
