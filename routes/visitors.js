da9b300e81efdd41b929965a73b12750e8b7631a
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-06-29 15:10:20 +0200

    create socketController

--- routes/visitors.js
+++ routes/visitors.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const profileController = require('../controllers/profileController')
 const visitorController = require('../controllers/visitorController')
-
-router.get('/', profileController.auth, visitorController.notif, visitorController.listVisitors)
+const socketController = require('../controllers/socketController')
+router.get('/', profileController.auth, socketController.notif, visitorController.listVisitors)
 
 router.get('/likes', profileController.auth,visitorController.notif,  visitorController.listLikes)
 
